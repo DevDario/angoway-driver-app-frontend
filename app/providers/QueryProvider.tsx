7176be3af86e7846b454ff63d5bc60cd7ab278af
DevDario <dariosilva13222@gmail.com>
2025-04-07 22:56:50 +0000

    feat: creates Query Client Provider

---
+++ app/providers/QueryProvider.tsx
@@ -0,0 +1,14 @@
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query"
+import { useState, ReactNode } from "react"
+import React from "react"
+
+
+
+export default function QueryProvider({children}:{children: ReactNode}) {
+    const [queryClient] = useState(() => new QueryClient())
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
