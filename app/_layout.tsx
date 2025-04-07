41a5b69edc77058bca60f47648d28127b76d0405
DevDario <dariosilva13222@gmail.com>
2025-04-07 23:10:48 +0000

    feat: wrapps content inside the query provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
+import QueryProvider from "./providers/QueryProvider";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name="index" options={{
+          title: "index"
+        }} />
+
+      </Stack>
+
+    </QueryProvider>
+  )
 }
