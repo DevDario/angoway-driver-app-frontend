f20ab40462d5b3d3ac98e5c9f45624468c61add2
DevDario <dariosilva13222@gmail.com>
2025-04-08 00:30:26 +0000

    feat: defines a custom useLogin hook with react-query mutation

---
+++ app/hooks/useAuth.tsx
@@ -0,0 +1,52 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { loginUseCase } from "../api/auth-usecases";
+import { useEffect, useState } from "react";
+import { getToken, saveToken } from "../utils/secure-store";
+import { useRouter } from "expo-router";
+import { LoginResponse } from "../types/login-response";
+
+export function useAuth() {
+    const queryClient = useQueryClient()
+    const router = useRouter()
+    const [authToken, setAuthToken] = useState<string | null>(null)
+    const [isCheckingAuth, setIsCheckingAuth] = useState<boolean>(true);
+    const [authError, setAuthError] = useState<string | null>(null);
+
+
+    useEffect(() => {
+        async function checkToken() {
+            const token: string | null | undefined = getToken();
+            setAuthToken(token || null);
+            setIsCheckingAuth(false)
+        }
+        checkToken()
+    }, []);
+
+    const useLogin = useMutation({
+        mutationFn: loginUseCase,
+        onMutate: () => {
+            setIsCheckingAuth(true)
+            setAuthError(null)
+        },
+        onSuccess: async (data: LoginResponse) => {
+            saveToken(data.access_token)
+            setAuthToken(data.access_token)
+            router.replace("/")
+            queryClient.invalidateQueries(["user"])
+        },
+        onError: async (req: any) => {
+            setAuthError(req.response.data.message)
+        },
+        onSettled: () => {
+            setIsCheckingAuth(false)
+        },
+    })
+
+    return {
+        useLogin,
+        authToken,
+        isCheckingAuth,
+        authError
+    }
+
+}
