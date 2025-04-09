afca67bbbcc7ad891985d404645482d8d87d9bd7
DevDario <dariosilva13222@gmail.com>
2025-04-09 05:41:54 +0000

    feat: creates a card to display the current route

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -5,7 +5,16 @@ import { View, Text, StyleSheet } from "react-native";
 export default function Index() {
     return(
         <View style={styles.container}>
-            <Text>Dashboard</Text>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Dashboard</Text>
+
+            </View>
+            <View style={styles.routeCard}>
+                <Text style={styles.routeCardLabel}>Rota Atual</Text>
+                <View style={styles.routeCardContent}>
+
+                </View>
+            </View>
         </View>
     )
 }
@@ -17,4 +26,32 @@ const styles = StyleSheet.create({
         paddingBottom: 30,
         marginHorizontal: 30,
       },
+      header: {
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingBottom: 20,
+      },
+      headerText:{
+        fontSize: 19,
+        fontWeight: "bold",
+        color: "#0C6BFF"
+      },
+      routeCard:{
+        width: "100%",
+        height: 130,
+        padding: 15,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: "#0C6BFF"
+      },
+      routeCardLabel:{
+        fontSize: 14,
+        fontWeight: 300,
+        color: "#333"
+      },
+      routeCardContent:{
+        
+      }
 })
