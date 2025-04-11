e9e97ec6deab2d2f46a101770571a3a173879a74
DevDario <dariosilva13222@gmail.com>
2025-04-11 22:12:16 +0000

    refactor: renames method

--- app/api/bus-usecases.ts
+++ app/api/bus-usecases.ts
@@ -3,7 +3,7 @@ import { api } from "./axios-instance";
 import { getToken } from "../utils/secure-store";
 
 
-export const getBusDetails = async ():Promise<busDetailsResponse> => {
+export const getBusDetailsUseCase = async ():Promise<busDetailsResponse> => {
     const token = getToken()
     const response = await api.get("/bus/dashboard-details",{
         headers:{
