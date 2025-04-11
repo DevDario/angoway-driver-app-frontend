ce49a67ab4755fa2b1e9701d1c7a2b2887c658ca
DevDario <dariosilva13222@gmail.com>
2025-04-11 21:54:38 +0000

    fix: sends the user token on the request

--- app/api/bus-usecases.ts
+++ app/api/bus-usecases.ts
@@ -1,8 +1,14 @@
 import { busDetailsResponse } from "../types/bus-details-response";
 import { api } from "./axios-instance";
+import { getToken } from "../utils/secure-store";
 
 
 export const getBusDetails = async ():Promise<busDetailsResponse> => {
-    const response = await api.get("/bus/dashboard-details")
+    const token = getToken()
+    const response = await api.get("/bus/dashboard-details",{
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    })
     return response.data
 }
