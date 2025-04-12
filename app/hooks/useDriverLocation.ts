3177156bcd5f878ef250939433a5c09a724cddc0
DevDario <dariosilva13222@gmail.com>
2025-04-12 18:31:07 +0000

    feat: creates a custom hook to subscribe to driver location broadcast

---
+++ app/hooks/useDriverLocation.ts
@@ -0,0 +1,39 @@
+import * as Location from "expo-location";
+import { useEffect, useRef } from "react";
+import { broadcastDriverLocationUseCase } from "../api/location-usecases";
+import { socket } from "../api/socket-instance";
+import { DriverLocation } from "../types/driver-location";
+
+export function useDriverLocation(shouldTrack: boolean = true) {
+  const locationSubscription = useRef<Location.LocationSubscription | null>(null);
+
+  useEffect(() => {
+    if (!shouldTrack) return;
+
+    socket.connect();
+
+    const startWatching = async () => {
+      locationSubscription.current = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.Highest,
+          timeInterval: 5000, // every 5 seconds
+          distanceInterval: 10, // or every 10 meters
+        },
+        (loc) => {
+          const driverLocation:DriverLocation = {
+            lat: loc.coords.latitude,
+            lng: loc.coords.longitude,
+          };
+          broadcastDriverLocationUseCase(driverLocation);
+        }
+      );
+    };
+
+    startWatching();
+
+    return () => {
+      locationSubscription.current?.remove();
+      socket.disconnect();
+    };
+  }, [shouldTrack]);
+}
