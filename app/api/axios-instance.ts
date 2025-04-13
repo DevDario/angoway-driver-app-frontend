a068a7b8ccc764c2f6ac1fd791700c215d171adc
Dário Silva <dariosilva13222@gmail.com>
2025-04-13 01:01:07 +0100

    fix: send requests without credentials

--- app/api/axios-instance.ts
+++ app/api/axios-instance.ts
@@ -2,7 +2,6 @@ import axios from "axios"
 
 export const api = axios.create({
     baseURL: process.env.EXPO_PUBLIC_BACKEND_URL || "https://angoway-app-backend.onrender.com",
-    withCredentials: true,
     headers: {
         "Content-Type": "application/json"
     },
