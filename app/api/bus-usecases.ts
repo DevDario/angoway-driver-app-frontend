ebdb8a7fe5882853279f91ea487ed9d3c5fe1476
Dário Silva <dariosilva13222@gmail.com>
2025-04-25 05:54:08 +0100

    feat: casts the response as BusDetailsResponse refactor: removes explicit method return type

--- app/api/bus-usecases.ts
+++ app/api/bus-usecases.ts
@@ -3,12 +3,12 @@ import { api } from "./axios-instance";
 import { getToken } from "../utils/secure-store";
 
 
-export const getBusDetailsUseCase = async ():Promise<busDetailsResponse> => {
+export const getBusDetailsUseCase = async () => {
     const token = await getToken()
     const response = await api.get("/bus/dashboard-details",{
         headers:{
             Authorization: `Bearer ${token}`
         }
     })
-    return response.data
+    return response.data as busDetailsResponse
 }
