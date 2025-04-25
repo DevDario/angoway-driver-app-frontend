d7cb98811de9f099e4cd95bc019fe07a75a8cde1
Dário Silva <dariosilva13222@gmail.com>
2025-04-25 04:28:19 +0100

    refactor: change IP Address

--- app/api/axios-instance.ts
+++ app/api/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://172.20.10.4:3000",
+    baseURL: "http://172.20.10.14:3000",
     headers: {
         "Content-Type": "application/json"
     },
