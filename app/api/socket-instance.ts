96dd6979a7f668bd424a1481590d647bba6fd369
Dário Silva <dariosilva13222@gmail.com>
2025-04-28 00:59:06 +0100

    fix: changes the backend URL

--- app/api/socket-instance.ts
+++ app/api/socket-instance.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { getToken } from "../utils/secure-store";
 
-const BACKEND_URL = "http://172.20.10.4:3000"
+const BACKEND_URL = "http://172.20.10.14:3000"
 
 const socket = io(BACKEND_URL, {
   autoConnect: false,
