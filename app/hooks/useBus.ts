82b13a3a0220af6defe82197301e561db4c61138
Dário Silva <dariosilva13222@gmail.com>
2025-04-28 02:19:02 +0100

    feat: creates a custom hook mutation for bus details update

--- app/hooks/useBus.ts
+++ app/hooks/useBus.ts
@@ -1,17 +1,30 @@
-import { useQuery } from "@tanstack/react-query";
-import { getBusDetailsUseCase } from "../api/bus-usecases";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  getBusDetailsUseCase,
+  updateBusDetailsUseCase,
+} from "../api/bus-usecases";
+import { useState } from "react";
 
 export function useBus() {
+  const [error, setError] = useState<string | null>(null);
+  const useBusDetails = useQuery({
+    queryKey: ["busDetails"],
+    queryFn: getBusDetailsUseCase,
+    staleTime: 1000 * 60 * 5,
+    cacheTime: 1000 * 60 * 10,
+  });
 
-    const useBusDetails = useQuery({
-        queryKey: ["busDetails"],
-        queryFn: getBusDetailsUseCase,
-        staleTime: 1000 * 60 * 5,
-        cacheTime: 1000 * 60 * 10
-    })
+  const useUpdateBusDetails = useMutation({
+    mutationFn: updateBusDetailsUseCase,
+    onSuccess: () => {},
+    onError: (req: any) => {
+      setError(req.response.data.message);
+    },
+  });
 
-    return {
-        useBusDetails
-    }
-
+  return {
+    useBusDetails,
+    useUpdateBusDetails,
+    error,
+  };
 }
