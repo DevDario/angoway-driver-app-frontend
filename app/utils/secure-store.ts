153672fb11db6566a4aecc3f791d2d2df5419f9f
Dário Silva <dariosilva13222@gmail.com>
2025-05-05 16:36:28 +0100

    feat: creates methods to decoded the token and to get the user ID from the token

--- app/utils/secure-store.ts
+++ app/utils/secure-store.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { jwtDecode } from "jwt-decode";
 
 const TOKEN_KEY: string = process.env.EXPO_PUBLIC_TOKEN_KEY || "access_token";
 
@@ -25,4 +26,22 @@ export async function removeToken() {
   } catch (error) {
     console.error("Error removing token:", error);
   }
+}
+
+export function decodeToken(
+  token: string
+): { sub: number; [key: string]: any } | null {
+  try {
+    return jwtDecode(token);
+  } catch (error) {
+    console.error("Error decoding token:", error);
+    return null;
+  }
+}
+
+export async function getUserId(): Promise<number | null> {
+  const token = await getToken();
+  if (!token) return null;
+  const decoded = decodeToken(token);
+  return decoded?.sub || null;
 }
