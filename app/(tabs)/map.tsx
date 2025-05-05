4d1ad24370d45564ae7116b7c0b8dbb67a93b80c
Dário Silva <dariosilva13222@gmail.com>
2025-05-05 11:02:15 +0100

    refactor: simplify Map component by removing location permission handling to dashboard page

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,31 +1,7 @@
-import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
-import { requestLocationPermission } from "../utils/request-location-permission";
-import { useDriverLocation } from "../hooks/useDriverLocation";
+import { View, StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 
 export default function Map() {
-  const [permissionGranted, setPermissionGranted] = useState(false);
-
-  useEffect(() => {
-    const requestPermissionForLocation = async () => {
-      const granted = await requestLocationPermission();
-      setPermissionGranted(granted);
-    };
-    requestPermissionForLocation();
-  }, []);
-
-  useDriverLocation(permissionGranted);
-
-  if (!permissionGranted) {
-    return (
-        <View style={styles.loadingContainer}>
-             <ActivityIndicator style={{justifyContent:"center", alignItems:"center", backgroundColor:"#FCFCB"}} size="large" color="#0C6BFF" />
-             <Text style={styles.permissionText}>Aguardando Permissão</Text>
-        </View>
-   );
-  }
-
   return (
     <View style={styles.container}>
       <MapView
@@ -44,26 +20,13 @@ export default function Map() {
   );
 }
 
-
 const styles = StyleSheet.create({
-    loadingContainer:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-        flexDirection:"column",
-        gap:10
-    },
-    permissionText:{
-        fontSize:14,
-        fontWeight:"bold",
-        color:"#0C6BFF"
-    },
-    container:{
-       flex:1
-    },
-    map:{
-        flex:1,
-        width:"100%",
-        height:"100%"
-    }
-})
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+});
