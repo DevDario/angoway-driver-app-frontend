84e452b7635eda2148010d2282e8618037ab244d
Dário Silva <dariosilva13222@gmail.com>
2025-06-05 06:16:06 +0100

    chore: adds static server link

--- app/api/axios-instance.ts
+++ app/api/axios-instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BACKEND_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
+const BACKEND_URL = "http://192.168.18.16:3000";
 
 export const api = axios.create({
     baseURL: BACKEND_URL,
--- app/api/socket-instance.ts
+++ app/api/socket-instance.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { getToken } from "../utils/secure-store";
 
-const BACKEND_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
+const BACKEND_URL = "http://192.168.18.16:3000";
 
 const socket = io(BACKEND_URL, {
   autoConnect: false,
