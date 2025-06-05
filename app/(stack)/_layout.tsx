a16ebb8a627d4e5624a3270894b6c4462ab2c39c
Dário Silva <dariosilva13222@gmail.com>
2025-06-05 12:57:46 +0100

    chore: imports custom fonts

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -1,9 +1,27 @@
 import { Stack } from "expo-router";
 import QueryProvider from "../providers/QueryProvider";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import { SplashScreen } from "expo-router";
 
 
 
 export default function AuthLayout() {
+  const [fontsLoaded] = useFonts({
+      Inter: require("../../assets/fonts/InterRegular.ttf"),
+      "Inter-Bold": require("../../assets/fonts/InterBold.ttf"),
+      "Inter-Light": require("../../assets/fonts/InterLight.ttf"),
+      "Inter-Medium": require("../../assets/fonts/InterMedium.ttf"),
+      "Inter-Regular": require("../../assets/fonts/InterRegular.ttf"),
+    });
+  
+    useEffect(() => {
+      if (fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded]);
+  
+    if (!fontsLoaded) {
+      return null;
+    }
     return (
         <QueryProvider>
             <Stack
